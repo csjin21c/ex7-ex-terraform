2780e76b21c0706d11136553bdc346a5c918c591
Joongjin <csjin21c@gmail.com>
2026-09-17 12:03:18 +0900

    깃액션을 이용한 인프라 배포(다중배포테스트)

--- infra/project/main.tf
+++ infra/project/main.tf
@@ -4,5 +4,8 @@ resource "aws_vpc" "this" {
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags                 = { Name = "csjin-test-vpc" }
+
+  assign_generated_ipv6_cidr_block     = false
+  enable_network_address_usage_metrics = false
+  tags                                 = { Name = "csjin-test-vpc" }
 }
